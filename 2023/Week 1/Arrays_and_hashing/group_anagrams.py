9df7ebf0d37891432e019e7501ca906e54bcdc6b
Don <thewayofone@gmail.com>
2023-07-29 17:09:22 -0700

    update group anagrams with O(m*n) solution

--- 2023/Week 1/Arrays_and_hashing/group_anagrams.py
+++ 2023/Week 1/Arrays_and_hashing/group_anagrams.py
@@ -11,3 +11,21 @@ class Solution:
         for word in strs:
             result["".join(sorted(word))].append(word)
         return result.values()
+    
+# O(m * n) where m is list size and n is word length
+# use a tuple of counts of each letter instead of sorting
+# we have to use a tuple because lists and dictionaries are not hashable so they can't be used as keys
+# in python this cause a TypeError: unhashable type: 'list' but in C# it would be a compile time error
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for letter in word:
+                count[ord(letter) - ord('a')] += 1
+            result[tuple(count)].append(word)
+        return result.values()
+    
+    
