6cd2f578d05f61caa53caa334feb4393334c7287
Don Lao <thewayofone@gmail.com>
2023-08-11 08:11:08 -0700

    update min stack

--- 2023/Week 2/Stack/min_stack.py
+++ 2023/Week 2/Stack/min_stack.py
@@ -1,23 +1,31 @@
 # based on https://leetcode.com/problems/min-stack/
-# min heap or priority queue using an array
+# can't use a min heap/priority queue here since the getMin is supposed to be O(1)
 
 class MinStack:
     
     def __init__(self):
         data = []
+        mins = []
 
     def push(self, val: int) -> None:
+        # if the new value is less than the current min, add it to the mins list\
+        # otherwise, add the current min to the mins list
+        if len(self.mins) == 0 or val < self.mins[-1]:
+            self.mins.append(val)
         self.data.append(val)
 
     def pop(self) -> None:
-        
+        # if the value being popped is the current min, pop it from the mins list
+        if self.data[-1] == self.mins[-1]:
+            self.mins.pop()
+        self.data.pop()
 
     def top(self) -> int:
-        
+        return self.data[-1]
 
     def getMin(self) -> int:
+        return self.mins[-1]
         
-    def _heapify(self):
         
 
 
